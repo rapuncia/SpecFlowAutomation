e96b2ff54ca0025e194efff069218c448999586c
rapuncia <keruzu@mail.ru>
2020-12-15 22:30:59 +0200

    test

---
+++ SpecFlowAutomation/Base/DriverManager.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecFlowAutomation.Base
+{
+    public class DriverManager
+    {
+        private static IWebDriver driver;
+
+        public static IWebDriver Instance()
+        {
+            if (driver == null)
+            {
+                driver = new ChromeDriver();
+                driver.Manage().Timeouts().ImplicitWait.Add(TimeSpan.FromSeconds(30));
+                driver.Manage().Window.Maximize();
+            }
+            return driver;
+        }
+
+        public static void QuitDriver()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
